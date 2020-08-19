6981cebaad187fac3a767860d3e6d620c7bcfd7a
PWRScript <powerofthescript@outlook.com>
2020-08-19 20:24:49 +0100

    Update PWRMod.java
    Fixed dependence errors

--- src/main/java/com/pwrscript/pwrmod/PWRMod.java
+++ src/main/java/com/pwrscript/pwrmod/PWRMod.java
@@ -10,7 +10,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sun.security.mscapi.CPublicKey;
 
 @Mod("pwrmod")
 public class PWRMod
